061ecc1133ee521aa00c094da6915cb3c836e8b8
Rhemerson.monteiro <rhemerson.monteiro@engegraph.com.br>
2024-06-20 18:45:47 -0300

    SavePoint: implementado projeto calculadora e matematicaTeste

---
+++ Calculadora/Matematica.cs
@@ -0,0 +1,50 @@
+﻿namespace Calculadora
+{
+    public class Matematica
+    {
+
+        public decimal Adicao(decimal value1, decimal value2) 
+        {
+            return value1 + value2;
+        }
+
+        public decimal Subtracao(decimal value1, decimal value2)
+        {
+            return value1 - value2; 
+        }
+
+        public decimal Multiplicacao(decimal value1, decimal value2)
+        {
+            return value1 * value2;
+        }
+
+        public decimal Divisao(decimal value1, decimal value2)
+        { 
+            return value1 / value2; 
+        }
+
+        public decimal Potencia(decimal value1, decimal potencia)
+        {
+            switch (potencia)
+            {
+                case 0:
+                {
+                    return 1;
+                }
+                case 1: 
+                {
+                    return value1;
+                }
+                default:
+                {
+                    decimal result = value1;
+                    for (int i = 1; i < potencia; i++) 
+                    {
+                            result *= value1;
+                    }
+                    return result;
+                }
+            }
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,71 @@
+﻿using Calculadora;
+
+Console.WriteLine();
+Console.WriteLine("Calculos basicos:");
+Console.WriteLine("Index      *");
+Console.WriteLine("  0        +");
+Console.WriteLine("  1        -");
+Console.WriteLine("  2        /");
+Console.WriteLine("  3        x");
+Console.WriteLine("  4        ^");
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine();
+        Console.Write("Informe o index da operacao: ");
+        string? resultado = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Informe o primeiro numero: ");
+        string? value1 = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Informe o segundo numero: ");
+        string? value2 = Console.ReadLine();
+        decimal valuUm;
+        decimal valuDois;
+        if (decimal.TryParse(value1, out valuUm) && decimal.TryParse(value2, out valuDois) && !string.IsNullOrEmpty(resultado))
+        {
+            Matematica matematica = new Matematica();
+            switch (resultado.ToUpper())
+            {
+                case "0":
+                    {
+                        Console.WriteLine("Resultado adição: {0}", matematica.Adicao(valuUm, valuDois));
+                        break;
+                    }
+                case "1":
+                    {
+                        Console.WriteLine("Resultado subtração: {0}", matematica.Subtracao(valuUm, valuDois));
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Resultado divisão: {0}", matematica.Divisao(valuUm, valuDois));
+                        break;
+                    }
+                case "3":
+                    {
+                        Console.WriteLine("Resultado multiplicação: {0}", matematica.Multiplicacao(valuUm, valuDois));
+                        break;
+                    }
+                case "4":
+                    {
+                        Console.WriteLine("Resultado potencia: {0}", matematica.Potencia(valuUm, valuDois));
+                        break;
+                    }
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    catch (Exception e) 
+    { 
+        Console.WriteLine(e.Message); 
+        break;
+    }
+}
+
+Console.ReadKey();
